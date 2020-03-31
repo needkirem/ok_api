cfaea948487e6c01ecc89d29653aad3cc88a56e2
needkirem <needkirem@gmail.com>
2020-03-31 23:02:28 +0500

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ok_api',
-    version='1.0.0',
+    version='1.0.1',
     description='Python библиотека для работы с API Одноклассников (API Wrapper ok.ru)',
 
     long_description=long_description,
